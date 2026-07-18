d3307d531977cf12f9a5706be325385474ba03b5
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2026-07-18 16:35:34 +0900

    Add employee detail link

--- src/app/employees/employee-list/employee-list.ts
+++ src/app/employees/employee-list/employee-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.scss',
 })
