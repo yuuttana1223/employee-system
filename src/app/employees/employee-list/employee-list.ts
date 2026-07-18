67631d0ddf6d201ce52bc46c33ada85793925738
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2026-07-18 16:56:12 +0900

    Display static employee data

--- src/app/employees/employee-list/employee-list.ts
+++ src/app/employees/employee-list/employee-list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { Employee } from '../employee';
+
 @Component({
   selector: 'app-employee-list',
   standalone: true,
@@ -8,4 +10,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.scss',
 })
-export class EmployeeList {}
+export class EmployeeList {
+  protected readonly employee: Employee = {
+    id: '1',
+    name: '山田 太郎',
+    department: '開発部',
+    position: 'Webエンジニア',
+  };
+}
