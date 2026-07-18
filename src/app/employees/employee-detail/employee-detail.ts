d533385366e9d457a1301f400a43548a2e296d9f
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2026-07-18 15:27:38 +0900

    Add employee detail component

---
+++ src/app/employees/employee-detail/employee-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-detail',
+  standalone: true,
+  templateUrl: './employee-detail.html',
+  styleUrl: './employee-detail.scss',
+})
+export class EmployeeDetail {}
