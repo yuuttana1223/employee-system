9967bffb9859790b33ce81d89e1267cffcc03f6d
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2026-07-18 15:34:30 +0900

    Add employee detail route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'employees/:employeeId',
+    loadComponent: () =>
+      import('./employees/employee-detail/employee-detail').then(
+        ({ EmployeeDetail }) => EmployeeDetail,
+      ),
+  },
+  {
     path: 'employees',
     loadComponent: () =>
       import('./employees/employee-list/employee-list').then(({ EmployeeList }) => EmployeeList),
