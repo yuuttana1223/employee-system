d82404614425b192efae3d4cba8bab893e3aa52b
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2026-08-01 00:32:38 +0900

    Display employee details from service

--- src/app/employees/employee-detail/employee-detail.ts
+++ src/app/employees/employee-detail/employee-detail.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { EmployeeService } from '../employee-service';
+
 @Component({
   selector: 'app-employee-detail',
   standalone: true,
@@ -9,7 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EmployeeDetail {
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly employeeService = inject(EmployeeService);
 
   protected readonly employeeId =
-    this.activatedRoute.snapshot.paramMap.get('employeeId') ?? '不明';
+    this.activatedRoute.snapshot.paramMap.get('employeeId') ?? '';
+
+  protected readonly employee = this.employeeService.getEmployeeById(
+    this.employeeId,
+  );
 }
